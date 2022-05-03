337367c5d3ac976b7dd6ceda765e101d49169019
davofredo <davofredo@gmail.com>
2022-05-03 10:58:45 -0500

    Fix path for csv files in AppConstants

--- src/main/java/com/at/internship/schedule/constants/AppConstants.java
+++ src/main/java/com/at/internship/schedule/constants/AppConstants.java
@@ -1,5 +1,7 @@
 package com.at.internship.schedule.constants;
 
+import java.io.File;
+
 public class AppConstants {
-    public static final String SERIALIZATION_PATH = System.getProperty("user.dir");
+    public static final String SERIALIZATION_PATH = System.getProperty("user.dir") + File.separator;
 }
