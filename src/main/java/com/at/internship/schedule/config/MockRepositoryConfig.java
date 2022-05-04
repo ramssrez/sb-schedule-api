e3cf4b3d5310aabd27322161742d0257dcc4d488
Raul Ramirez Perez <ramssrez@gmail.com>
2022-05-04 12:14:50 +0200

    Finalizacion de la configuración del csv y los mocks

--- src/main/java/com/at/internship/schedule/config/MockRepositoryConfig.java
+++ src/main/java/com/at/internship/schedule/config/MockRepositoryConfig.java
@@ -9,11 +9,14 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MockRepositoryConfig {
-    @Bean
+    public static final String BEAN_CONTACT_REPOSITORY="mockContactRepository";
+    public static final String BEAN_APPOINTMENT_REPOSITORY="mockAppointmentRepository";
+
+    @Bean(name = BEAN_CONTACT_REPOSITORY)
     public IContactRepository mockContactRepository(){
         return new MockContactRepositoryImpl();
     }
-    @Bean
+    @Bean(name = BEAN_APPOINTMENT_REPOSITORY)
     public IAppointmentRepository mockAppointmentRepository(){
         return new MockAppointmentRepositoryImpl();
     }
--- src/main/java/com/at/internship/schedule/service/impl/ContactServiceImpl.java
+++ src/main/java/com/at/internship/schedule/service/impl/ContactServiceImpl.java
@@ -1,8 +1,12 @@
 package com.at.internship.schedule.service.impl;
 
+import com.at.internship.schedule.config.CsvRepositoryConfig;
+import com.at.internship.schedule.config.MockRepositoryConfig;
 import com.at.internship.schedule.domain.Contact;
 import com.at.internship.schedule.repository.IContactRepository;
+import com.at.internship.schedule.repository.impl.MockContactRepositoryImpl;
 import com.at.internship.schedule.service.IContactService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +18,7 @@ public class ContactServiceImpl implements IContactService {
     private final IContactRepository contactRepository;
 
     public ContactServiceImpl(
+            @Qualifier(MockRepositoryConfig.BEAN_CONTACT_REPOSITORY)
             IContactRepository contactRepository
     ) {
         this.contactRepository = contactRepository;
