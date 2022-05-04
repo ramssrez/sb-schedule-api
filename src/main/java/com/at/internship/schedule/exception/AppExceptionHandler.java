3710f0c5f0481dc918cc514d080b3290a11a8641
Raul Ramirez Perez <ramssrez@gmail.com>
2022-05-04 16:51:26 +0200

    Creación de la exception por entrada de parametros

--- src/main/java/com/at/internship/schedule/exception/AppExceptionHandler.java
+++ src/main/java/com/at/internship/schedule/exception/AppExceptionHandler.java
@@ -6,12 +6,22 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Collections;
+
 @ControllerAdvice
 public class AppExceptionHandler {
+    private static final String STR_NESTED_EXCEPTION = "nested exception is";
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseBody
     public ErrorResponse handleDefaultHandlerExceptionResolver(HttpMessageNotReadableException e){
         e.printStackTrace();
-        return new ErrorResponse();
+        String errorMessage = e.getMessage();
+        if (errorMessage != null && e.getMessage().contains(STR_NESTED_EXCEPTION))
+            errorMessage = errorMessage.substring(0,errorMessage.indexOf(STR_NESTED_EXCEPTION));
+        ErrorResponse response = new ErrorResponse();
+        response.setCode("Invalid_Input");
+        response.setMesssage("Bad Request");
+        response.setErrorMessage(Collections.singletonList(errorMessage));
+        return response;
     }
 }
--- src/main/java/com/at/internship/schedule/response/ErrorResponse.java
+++ src/main/java/com/at/internship/schedule/response/ErrorResponse.java
@@ -1,7 +1,10 @@
 package com.at.internship.schedule.response;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class ErrorResponse extends GenericResponse{
     List<String> errorMessage;
 }
