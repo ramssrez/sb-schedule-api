1fd92787e10345c39d82b4973ad6c134b42e20e3
Raul Ramirez Perez <ramssrez@gmail.com>
2022-05-18 11:05:26 +0200

    create the table contact_phone in the sb

--- src/main/java/com/at/internship/schedule/domain/ContactPhone.java
+++ src/main/java/com/at/internship/schedule/domain/ContactPhone.java
@@ -2,9 +2,20 @@ package com.at.internship.schedule.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "contact_phone")
 public class ContactPhone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_id")
     private Integer contactId;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(name = "label")
     private String label;
 }
